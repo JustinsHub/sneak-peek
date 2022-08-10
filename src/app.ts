3ed4517a49f2e1916e6ec7baf58d38e8ed6c0ca6
Justin <contactjustinzamora@gmail.com>
2022-08-10 12:03:35 -0700

    installed cookie-parser and implemented

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
 import userRoutes from './routes/userRoutes'
 import { errorHandlerMiddleware } from './middleware/errorHandlerMiddleware'
@@ -9,6 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cors())
+app.use(cookieParser())
 
 app.use('/', userRoutes)
 
--- src/config.ts
+++ src/config.ts
@@ -3,8 +3,10 @@ dotenv.config()
 
 const PORT = process.env.PORT
 const BCRYPT_WORK_FACTOR = process.env.BCRYPTO_WORK_FACTOR || 10
+const SECRET_KEY = process.env.SECRET_KEY
 
 export {
     PORT,
-    BCRYPT_WORK_FACTOR
+    BCRYPT_WORK_FACTOR,
+    SECRET_KEY
 }
